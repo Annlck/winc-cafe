e4ceeb689abc0e2cafc8b687b0153be0de9c4f3e
Ann1ck <annick@henkes.it>
2025-08-07 17:30:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import { DrinkButtons } from "./components/DrinkButtons";
+import { DrinkChoice } from "./components/ui/DrinkChoice";
 import { coffee, tea } from "./utils/data";
 
 export const App = () => {
   const greeting = "Welcome";
+  const userDrink = tea;
 
   return (
     <>
       <h1>{greeting}</h1>
       <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
+      <DrinkChoice drink={userDrink} />
     </>
   );
 };
