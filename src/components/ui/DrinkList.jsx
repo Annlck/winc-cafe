4272ac0f268b20faf8f375df5fe82425337e1c5d
Ann1ck <annick@henkes.it>
2025-08-28 14:55:42 +0200

    Fix bug in DrinkList

--- src/components/ui/DrinkList.jsx
+++ src/components/ui/DrinkList.jsx
@@ -4,7 +4,7 @@ export const DrinkList = ({ allDrinks }) => {
   return (
     <>
       {allDrinks.map((item) => (
-        <DrinkItem drink={item} key={drink.id} />
+        <DrinkItem key={item.id} drink={item} />
       ))}
     </>
   );
