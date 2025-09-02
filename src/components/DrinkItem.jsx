6179c2165cfeb4116794f25b65585eec32e9c7be
Ann1ck <annick@henkes.it>
2025-09-02 17:16:55 +0200

    Update DrinkItem

--- src/components/DrinkItem.jsx
+++ src/components/DrinkItem.jsx
@@ -1,10 +1,26 @@
-import "./DrinkItem.css";
+import { Image } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 
 export const DrinkItem = ({ drink, clickFn }) => {
   return (
-    <button class="drink-item" onClick={() => clickFn(drink)}>
-      <img src={drink.imgUrl} alt={drink.alt} />
-      <p>{drink.name}</p>
-    </button>
+    <Flex
+      onClick={() => clickFn(drink)}
+      bgColor={"hsl(34, 64%, 98%)"}
+      width={"100%"}
+      cursor={"pointer"}
+      margin={"1rem"}
+      flexWrap={"wrap"}
+      flexDirection={"horizontal"}
+      borderRadius={"2rem"}
+      padding={"0.5rem"}
+      alignItems={"center"}
+      _hover={{ bgColor: "hsl(136, 26%, 92%)" }}
+    >
+      <Image src={drink.imgUrl} alt={drink.alt} boxSize="80px" />
+      <Text paddingLeft={"2rem"} fontSize={"xl"}>
+        {drink.name}
+      </Text>
+    </Flex>
   );
 };
--- src/components/DrinkList.jsx
+++ src/components/DrinkList.jsx
@@ -1,11 +1,12 @@
 import { DrinkItem } from "./DrinkItem";
+import { Container } from "@chakra-ui/react";
 
 export const DrinkList = ({ allDrinks, clickFn }) => {
   return (
-    <>
+    <Container centerContent>
       {allDrinks.map((item) => (
         <DrinkItem key={item.id} drink={item} clickFn={clickFn} />
       ))}
-    </>
+    </Container>
   );
 };
