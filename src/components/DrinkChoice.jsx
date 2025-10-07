f6137372223d11a9e0d7cf2b270303d960b03e69
Ann1ck <annick@henkes.it>
2025-10-07 18:38:56 +0200

    Add Modal to DrinkChoice

--- src/components/DrinkChoice.jsx
+++ src/components/DrinkChoice.jsx
@@ -4,22 +4,61 @@ import { Image } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
 import { Center } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 export const DrinkChoice = ({ drink, clickFn }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <>
+    <Center flexDir={"column"} gap={4}>
       <Heading size="xl" mb="2rem" color="blue.200">
         {drink.name}
       </Heading>
-      <Center>
-        <Image src={drink.imgUrl} alt={drink.alt} boxSize="300px" />
-      </Center>
+
+      <Image src={drink.imgUrl} alt={drink.alt} boxSize="300px" />
       <Text>
         Your {drink.name.toLowerCase()} will be ready in a few minutes.
       </Text>
-      <Button onClick={() => clickFn()} variant="ghost">
-        Change selection
-      </Button>
-    </>
+
+      <Flex mt={4}>
+        <Button onClick={onOpen} mr={4}>
+          Confirm order
+        </Button>
+        <Button onClick={() => clickFn()} variant="ghost">
+          Change selection
+        </Button>
+      </Flex>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Confirm your order</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>1x {drink.name}</Text>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="teal" mr={4}>
+              Confirm
+            </Button>
+            <Button variant="ghost" onClick={onClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Center>
   );
 };
