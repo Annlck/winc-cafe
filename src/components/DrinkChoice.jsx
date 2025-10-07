2fc12de3bc47c8430c897dde7d3ed71531b33a94
Ann1ck <annick@henkes.it>
2025-10-07 15:39:08 +0200

    Update DrinkChoice (chakra)

--- src/components/DrinkChoice.jsx
+++ src/components/DrinkChoice.jsx
@@ -1,11 +1,22 @@
 import { Button } from "./ui/Button";
 
+import { Image } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
+import { Center } from "@chakra-ui/react";
+
 export const DrinkChoice = ({ drink, clickFn }) => {
   return (
     <>
-      <h2>{drink.name}</h2>
-      <img src={drink.imgUrl} alt={drink.alt}></img>
-      <p>Your {drink.name} will be ready in a few minutes.</p>
+      <Heading size="xl" mb="2rem" color="blue.200">
+        {drink.name}
+      </Heading>
+      <Center>
+        <Image src={drink.imgUrl} alt={drink.alt} boxSize="300px" />
+      </Center>
+      <Text>
+        Your {drink.name.toLowerCase()} will be ready in a few minutes.
+      </Text>
       <Button text={"Change selection"} clickFn={() => clickFn()} />
     </>
   );
